06a8364da529c92aaf60465cdb6c2bb531d6c811
AllanD1 <allanweb019@gmail.com>
2019-04-09 22:38:13 -0300

    Init in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Index from "./screens/index/Index";
+import About from "./screens/about/About";
+import Users from "./screens/users/Users";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
       </div>
+    </Router>
     );
   }
 }
---
+++ src/screens/index/Index.js
@@ -0,0 +1,9 @@
+import  React  from 'react';
+
+
+const Index = ()=>(
+        <h1>Index</h1>
+    );
+
+
+export default Index;
